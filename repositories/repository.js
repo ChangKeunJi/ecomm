d779bae0097f33207a5ff915dae178cfe6a493cd
ChangKeun <ckdrmsdk@gmail.com>
2021-03-23 12:38:22 +0900

    Remove cart item

--- repositories/repository.js
+++ repositories/repository.js
@@ -1,101 +1,98 @@
-const fs = require('fs');
-const crypto = require('crypto');
+const fs = require("fs");
+const crypto = require("crypto");
 
 module.exports = class Repository {
+  constructor(filename) {
+    //! 만약 매개변수가 없다면 THROW ERROR
+    if (!filename) throw new Error("NO FILE EXISTS");
 
-    constructor(filename) {
-     
-        //! 만약 매개변수가 없다면 THROW ERROR
-        if(!filename) throw new Error ('NO FILE EXISTS');
+    this.filename = filename;
 
-        this.filename = filename;
+    //! file이 존재하는지 확인한다.
+    // - accessSync를 사용하는 이유.
+    // 1. 딱 한번만 인스턴스를 생성한다. ( 자주사용 x)
+    // 2. constructor는 async 설정할 수 없다.
 
-        //! file이 존재하는지 확인한다. 
-        // - accessSync를 사용하는 이유.
-        // 1. 딱 한번만 인스턴스를 생성한다. ( 자주사용 x)
-        // 2. constructor는 async 설정할 수 없다. 
+    try {
+      fs.accessSync(this.filename);
+      // Check if file exists.
+      // if exist, returns undefined otherwise throws error.
+    } catch (err) {
+      // 만약 파일이 존재하지 않는다면 새로 만든다.
+      fs.writeFileSync(this.filename, "[]");
+    }
+  }
 
-        try {
-            fs.accessSync(this.filename);
-            // Check if file exists.
-            // if exist, returns undefined otherwise throws error.
-        } catch(err) {
-            // 만약 파일이 존재하지 않는다면 새로 만든다. 
-            fs.writeFileSync(this.filename,"[]");
+  async create(attrs) {
+    attrs.id = this.randomId();
 
+    const records = await this.getAll();
+    records.push(attrs);
+
+    await this.writeAll(records);
+
+    return attrs;
+  }
+
+  // Get a list of all users. JSON => JS
+  async getAll() {
+    return JSON.parse(
+      await fs.promises.readFile(this.filename, { encoding: "utf8" })
+    );
+  }
+
+  async writeAll(records) {
+    // Write the updated 'records' array back to this.filename
+    await fs.promises.writeFile(
+      this.filename,
+      JSON.stringify(records, null, 2)
+    );
+  }
+
+  randomId() {
+    return crypto.randomBytes(4).toString("hex");
+  }
+
+  async getOne(id) {
+    const records = await this.getAll();
+    return records.find((records) => records.id === id);
+  }
+
+  async delete(id) {
+    const records = await this.getAll();
+    const filteredRecords = records.filter((record) => record.id !== id);
+
+    await this.writeAll(filteredRecords);
+  }
+
+  async update(id, attrs) {
+    const records = await this.getAll();
+    const record = records.find((record) => record.id === id);
+
+    if (!record) {
+      throw new Error(`Record with id ${id} not found`);
+    }
+
+    Object.assign(record, attrs);
+
+    await this.writeAll(records);
+  }
+
+  async getOneBye(filters) {
+    const records = await this.getAll();
+
+    for (let record of records) {
+      let found = true;
+
+      for (let key in filters) {
+        if (record[key] !== filters[key]) {
+          found = false;
         }
+      }
+
+      if (found) {
+        return record;
+      }
     }
-
-    async create(attrs) {
-
-        attrs.id = this.randomId();
-
-        const records = await this.getAll();
-        records.push(attrs);
-        
-        await this.writeAll(records);
-
-        return attrs;
-    }
-
-    // Get a list of all users. JSON => JS
-    async getAll() {
-        return JSON.parse(await fs.promises.readFile(this.filename, {encoding:'utf8'}))
-    }
-  
-    async writeAll(records) {
-        // Write the updated 'records' array back to this.filename
-        await fs.promises.writeFile(this.filename, JSON.stringify(records,null,2));
-    }
-
-    randomId() {
-        return crypto.randomBytes(4).toString('hex');
-    }
-
-    async getOne(id) {
-        const records = await this.getAll();
-        return records.find(records => records.id === id);
-    }
-
-    async delete(id) {
-        const records = await this.getAll();
-        const filteredRecords = records.filter(record => record.id !== id);
-
-        await this.writeAll(filteredRecords)
-
-    }
-
-    async update(id,attrs) {
-        const records = await this.getAll();
-        const record = records.find(record => record.id === id);
-
-        if(!record) {
-            throw new Error(`Record with id ${id} not found`);
-        }
-
-        Object.assign(record,attrs);  
-
-        await this.writeAll(records);
-    }
-
-
-    async getOneBye(filters) {
-
-        const records = await this.getAll();
-
-        for (let record of records) {
-            let found = true;
-
-            for (let key in filters) {
-                if(record[key] !== filters[key]) {
-                    found = false;
-                }
-            }
-
-            if(found) {
-                return record;
-            }
-        }
-
-    }
-}
+  }
+};
--- routes/carts.js
+++ routes/carts.js
@@ -1,13 +1,15 @@
 const express = require("express");
 
 const cartsRepo = require("../repositories/carts");
+const productRepo = require("../repositories/products");
+const cartShowTemplate = require("../views/carts/show");
 
 const router = express.Router();
 
-// Receive a POST request to add an item to a cart
+//! Receive a POST request to add an item to a cart
 
 router.post("/cart/products", async (req, res) => {
-  // Figure out the cart
+  // 1) Figure out the cart
 
   let cart;
   if (!req.session.cartId) {
@@ -21,16 +23,61 @@ router.post("/cart/products", async (req, res) => {
     cart = await cartsRepo.getOne(req.session.cartId);
   }
 
-  console.log(cart);
-
-  // Either increment quantity for existing product
+  // 2) Either increment quantity for existing product
   // OR add new product to items array
 
-  res.send("Product Added!!");
+  const existingItem = cart.items.find(
+    (item) => req.body.productId === item.id
+  );
+
+  if (existingItem) {
+    // increment quantity and save cart
+    existingItem.quantity++;
+  } else {
+    // add new product id to items array
+    cart.items.push({ id: req.body.productId, quantity: 1 });
+  }
+
+  await cartsRepo.update(cart.id, {
+    items: cart.items,
+  });
+
+  res.redirect("/cart");
 });
 
-// Receive a GET request to show all the items in cart
+//! Receive a GET request to show all the items in cart
 
-// Receive a POST request to delete an item from a cart
+router.get("/cart", async (req, res) => {
+  if (!req.session.cartId) {
+    return res.redirect("/");
+  }
+
+  const cart = await cartsRepo.getOne(req.session.cartId);
+
+  for (let item of cart.items) {
+    const product = await productRepo.getOne(item.id);
+
+    item.product = product;
+  }
+
+  // Modified cart object won't be updated in JSON.
+
+  // res.send("CART LIST");
+  res.send(cartShowTemplate({ items: cart.items }));
+});
+
+//! Receive a POST request to delete an item from a cart
+
+router.post("/cart/products/delete", async (req, res) => {
+  const cart = await cartsRepo.getOne(req.session.cartId);
+
+  const items = cart.items.filter((item) => {
+    return item.id !== req.body.itemId;
+  });
+
+  await cartsRepo.update(cart.id, { items });
+
+  res.redirect("/cart");
+});
 
 module.exports = router;
